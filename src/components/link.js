7fa8cd5f044b4f186cc85d9a0f0ed9b548738c74
IamCyrek <maychenko1998@gmail.com>
2018-06-24 20:29:23 +0300

    new branch

--- src/components/link.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Link(props) {
-    return (
-        <a href={props.href} className={props.class}>{props.value}</a>
-    );
-}
--- src/components/pic.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Pic(props) {
-    return (
-        <img src={props.src} alt={props.value} className={props.class}/>
-    );
-}
