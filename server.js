00d280d02b3351f5a416bc0c19f26078c47449a4
IamCyrek <maychenko1998@gmail.com>
2018-10-29 02:11:43 +0300

    fix style

--- server.js
+++ server.js
@@ -4,36 +4,35 @@ const constants = require('./config/constants');
 
 const app = express();
 app.use(router);
-const server = app.listen(constants.PORT, () =>
-    console.log(constants.API_START)
-);
+const server = app.listen(constants.PORT, () => console.log(constants.API_START));
 
 setInterval(() => server.getConnections(
-    (err, connections) => console.log(`${connections}` + constants.CONNECTIONS_OPEN)
+  (err, connections) => console.log(`${connections}${constants.CONNECTIONS_OPEN}`),
 ), constants.SECOND);
-
-process.on(constants.SIGTERM_SIGNAL, shutDown);
-process.on(constants.SIGINT_SIGNAL, shutDown);
 
 let connections = [];
 
-server.on(constants.CONNECTION, connection => {
-    connections.push(connection);
-    connection.on(constants.CLOSE, () => connections = connections.filter(curr => curr !== connection));
+server.on(constants.CONNECTION, (connection) => {
+  connections.push(connection);
+  connection.on(constants.CLOSE,
+    () => connections = connections.filter(curr => curr !== connection));
 });
 
 function shutDown() {
-    console.log(constants.API_CLOSE);
-    server.close(() => {
-        console.log(constants.CLOSED_OUT);
-        process.exit(constants.OK_CODE);
-    });
+  console.log(constants.API_CLOSE);
+  server.close(() => {
+    console.log(constants.CLOSED_OUT);
+    process.exit(constants.OK_CODE);
+  });
 
-    setTimeout(() => {
-        console.error(constants.CLOSE_ERROR);
-        process.exit(constants.ERROR_CODE);
-    }, constants.TEN_SECONDS);
+  setTimeout(() => {
+    console.error(constants.CLOSE_ERROR);
+    process.exit(constants.ERROR_CODE);
+  }, constants.TEN_SECONDS);
 
-    connections.forEach(curr => curr.end());
-    setTimeout(() => connections.forEach(curr => curr.destroy()), constants.FIVE_SECONDS);
+  connections.forEach(curr => curr.end());
+  setTimeout(() => connections.forEach(curr => curr.destroy()), constants.FIVE_SECONDS);
 }
+
+process.on(constants.SIGTERM_SIGNAL, shutDown);
+process.on(constants.SIGINT_SIGNAL, shutDown);
--- services/services.js
+++ services/services.js
@@ -2,40 +2,37 @@ const movies = require('../data.json');
 const constants = require('../config/constants');
 
 function getAll() {
-    return movies;
+  return movies;
 }
 
 function getById(id) {
-    return movies.find(item => item.id === id);
-
+  return movies.find(item => item.id === id);
 }
 
 function getByTitle(title) {
-    return movies.filter(item => item.title.toLowerCase().includes(title.toLowerCase()));
+  return movies.filter(item => item.title.toLowerCase().includes(title.toLowerCase()));
 }
 
 function getByPagination(offset, limit) {
-    return movies.slice(offset, offset + limit);
+  return movies.slice(offset, offset + limit);
 }
 
 function getBySorting(field, direction) {
-    const list = movies.sort((a, b) => {
-        return String(a[field]).localeCompare(String(b[field]));
-    });
+  const list = movies.sort((a, b) => String(a[field]).localeCompare(String(b[field])));
 
-    if (!String(direction).toLowerCase().localeCompare(constants.ASCENDING_ORDER))
-        return list;
+  if (!String(direction).toLowerCase().localeCompare(constants.ASCENDING_ORDER)) return list;
 
-    if (!String(direction).toLowerCase().localeCompare(constants.DESCENDING_ORDER))
-        return list.reverse();
+  if (!String(direction)
+    .toLowerCase()
+    .localeCompare(constants.DESCENDING_ORDER)) return list.reverse();
 
-    return [];
+  return [];
 }
 
 module.exports = {
-    getAll,
-    getById,
-    getByTitle,
-    getByPagination,
-    getBySorting
+  getAll,
+  getById,
+  getByTitle,
+  getByPagination,
+  getBySorting,
 };
